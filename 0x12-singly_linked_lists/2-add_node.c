b38706031a030fdfd0981ff4eeacbce8576f46eb
Eman-salem123 <emanzatonaa@gmail.com>
2023-09-21 14:47:50 -0700

    lol

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,52 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- *  * add_node - a function that adds a new node at
- *   * the beginning of a list_t list.
- *    * @head: node head.
- *     * @str: node string.
- *      * Return: the address of the new element,
- *       * or NULL if it failed.
-        */
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *string;
-	list_t *node;
-	if (!head || !str)
-	{
-		return (0);
-	}
-	string = strdup(str);
-	if (!string)
-	{
-		return (0);
-	}
-	node = malloc(sizeof(list_t));
-	if (!node)
-	{
-		return (0);
-	}
-	node->str = string;
-	node->len = strlen(string);
-	node->next = *head;
-	*head = node;
-	return (node);
-}
-/**
- *  * _strlen - a function that swaps integers wih pointers.
- *   * @s: is a pointer to a char.
- *    * Return: EXIT_SUCCESS.
-     */
-int _strlen(char *s)
-{
-	int x = 0;
-	if (!s)
-	{
-		return (0);
-	}
-	while (*(s + x) != '\0')
-	{
-		x++;
-	}
-	return (x);
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
